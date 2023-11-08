26ea660d78b756fe74442103420178a705ab5735
Raim777 <rako2002@mail.ru>
2023-11-08 17:39:40 +0600

    providers added

---
+++ src/FluxNotificationsServiceProvider.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Raim\FluxNotif;
+
+use Filament\Facades\Filament;
+use Filament\Navigation\NavigationBuilder;
+use Filament\Navigation\NavigationGroup;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Collection;
+
+class FluxCatalogServiceProvider extends ServiceProvider
+{
+
+    public function boot()
+    {
+        // Catalog::observe(new CatalogObserver());
+
+        // if ($this->app->runningInConsole()) {
+        //     $this->publishes([
+        //         __DIR__ . '/../config/flux-catalog.php' => config_path('flux-catalog.php'),
+        //     ], 'flux-catalog-config');
+
+        //     $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        //     $this->publishMigrations();
+        // }
+
+        // $this->loadRoutesFrom(__DIR__ . '/../routes/api.php');
+    }
+
+    public function register()
+    {
+        // $this->app->bind('textConverter', TextConverterHelper::class);
+    }
+
+    /**
+     * Returns existing migration file if found, else uses the current timestamp.
+     */
+    protected function getMigrationFileName($index,string $migrationFileName): string
+    {
+        $timestamp = date('Y_m_d_His');
+
+        $filesystem = $this->app->make(Filesystem::class);
+
+        return Collection::make([$this->app->databasePath() . DIRECTORY_SEPARATOR . 'migrations' . DIRECTORY_SEPARATOR])
+            ->flatMap(fn($path) => $filesystem->glob($path . '*_' . $migrationFileName))
+            ->push($this->app->databasePath() . "/migrations/{$timestamp}{$index}_{$migrationFileName}")
+            ->first();
+    }
+}
